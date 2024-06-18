8a4929bfc8f3edd1902b4ff9fddb701874f79f4b
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-18 11:59:55 +0530

    Update Finding range 4.c
    Update Finding range 4.c

--- Finding range 4.c
+++ Finding range 4.c
@@ -1,5 +1,5 @@
 //QUESTION:--
-Write a C program that reads an integer and checks the specified range to which it belongs. Print an error message if the number is negative and greater than 80.
+//Write a C program that reads an integer and checks the specified range to which it belongs. Print an error message if the number is negative and greater than 80.
 
 //ANSWER:--
 #include <stdio.h>
